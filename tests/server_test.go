a1e70c3421828014664977bdf156e246710b4293
SlavaUtesinov <utesinov.slava@gmail.com>
2022-03-31 16:50:43 +0300

    fix

--- tests/server_test.go
+++ tests/server_test.go
@@ -19,21 +19,26 @@ func Test_Product_Should_Be_Created(t *testing.T) {
 
 	productIn := models.Product{Id: 1, Price: 123.123, Name: "DVD"}
 	serialized, _ := json.Marshal(productIn)
+	url := fmt.Sprintf("http://localhost:%v/api/products", port)
 
-	if response, err := http.Post(fmt.Sprintf("http://localhost:%v/api/products", port), "application/json", bytes.NewBuffer(serialized)); err == nil {
+	if _, err := http.Post(url, "application/json", bytes.NewBuffer(serialized)); err != nil {
+		t.Errorf("Error has happened during POST request: %v", err)
+	}
+
+	if response, err := http.Get(url); err == nil {
 		defer response.Body.Close()
 
 		decoder := json.NewDecoder(response.Body)
-		var productOut models.Product
+		productsOut := make([]models.Product, 0)
 
-		if err := decoder.Decode(&productOut); err == nil {
-			if productIn != productOut {
-				t.Errorf("Products are not equal: %v, %v", productIn, productOut)
+		if err := decoder.Decode(&productsOut); err == nil {
+			if productIn != productsOut[0] {
+				t.Errorf("Products are not equal: %v, %v", productIn, productsOut[0])
 			}
 		} else {
 			t.Errorf("Error has happened during deserialization: %v", err)
 		}
 	} else {
-		t.Errorf("Error has happened during HTTP request: %v", err)
+		t.Errorf("Error has happened during GET request: %v", err)
 	}
 }
